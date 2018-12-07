272878a94fbe2f2139a78be7192745beb25e5cb2
Alexander Dominguez <alexrendon2109@gmail.com>
2018-12-06 22:15:59 -0500

    Ejected app in order to use css modules.

--- gui/src/containers/App/App.jsx
+++ gui/src/containers/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './styles.scss';
+// import PropTypes from 'prop-types';
+import styles from './styles.css';
 
 const App = (/* { children } */) => (
   <div>
